4f1d0d3942ab6c8cb59c9dc4d9d2bc2ee7babebf
Vo Huy <huy.vovan@ncc.asia>
2022-05-25 15:22:33 +0700

    done sidebar

--- frontend/src/components/Sidebar/index.jsx
+++ frontend/src/components/Sidebar/index.jsx
@@ -1,20 +1,18 @@
 /* eslint-disable object-curly-newline */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { Divider, Grid } from '@mui/material';
-import React, { useContext } from 'react';
+import React from 'react';
 // eslint-disable-next-line import/no-cycle
-import { AppContext } from '../../App';
 import MenuListCategory from '../MenuListCategory';
 import MenuListSupplier from '../MenuListSupplier';
 import styles from './index.module.sass';
 
 function SideBar() {
-  const { suppliers } = useContext(AppContext);
   return (
     <Grid xs={2} className={styles.sidebar}>
       <MenuListCategory />
       <Divider />
-      <MenuListSupplier suppliers={suppliers} />
+      <MenuListSupplier />
     </Grid>
   );
 }
